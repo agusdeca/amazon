f9aaaa6f68db55aecf184356e4f42be2411bb26d
Agusdecaa <aguxxx10@gmail.com>
2024-08-11 17:17:07 -0300

    Creacion clase carrito

---
+++ javascript-amazon-project-main/data/class-carrito.js
@@ -0,0 +1,85 @@
+class Cart{
+    carrito= JSON.parse(localStorage.getItem(this.localStorageKey)) || [];
+    localStorageKey;
+
+    constructor(nombreLocalStorage){
+        this.localStorageKey=nombreLocalStorage;
+    }
+
+    almacenarCarrito() {
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.carrito));
+    };
+
+    agregarCarrito(idPr, cantidadPr) {
+        var articuloMatch;
+
+        this.carrito.forEach((item) => {
+            if (item.idProducto === idPr) {
+                articuloMatch = item;  
+            }
+        });
+
+        if (articuloMatch) {
+            articuloMatch.cantidadProducto += cantidadPr;
+        } else {
+            this.carrito.push({
+                idProducto: idPr,
+                cantidadProducto: cantidadPr,
+                deliveryId: '1'
+            });
+        }
+        
+        this.almacenarCarrito();
+        this.mostrarCarrito();
+    };
+
+    mostrarCarrito() {
+        var total = 0;
+        this.carrito.forEach((producto) => {
+            total += producto.cantidadProducto;
+        });
+
+        var cartQuantityElement = document.querySelector(".cart-quantity");
+        if (cartQuantityElement) {
+            cartQuantityElement.innerHTML = total;
+        } else {
+            console.error("El elemento .cart-quantity no se encontró en el DOM.");
+        }
+    };
+
+    borrarProducto(idProducto) {
+        const nuevoCarrito = this.carrito.filter((producto) => producto.idProducto !== idProducto);
+        this.carrito = nuevoCarrito;
+        this.almacenarCarrito();
+        this.actualizarCantidad();
+    };
+
+    actualizarCantidad() {
+        var returnToHomeLink = document.querySelector(".return-to-home-link");
+        if (returnToHomeLink) {
+            returnToHomeLink.innerHTML = `${this.carrito.length} items`;
+        } else {
+            console.error("El elemento .return-to-home-link no se encontró en el DOM.");
+        }
+    };
+
+    actualizarDelivery(idProducto, opcionDelivery) {
+        var articuloMatch;
+        this.carrito.forEach((item) => {
+            if (item.idProducto === idProducto) {
+                articuloMatch = item;  
+            }
+        });
+
+        if (articuloMatch) {
+            articuloMatch.deliveryId = opcionDelivery;
+            this.almacenarCarrito();
+        } else {
+            console.error('Producto no encontrado en el carrito.');
+        }
+    }
+}
+4
+const carro= new Cart("carrito-poo")
+carro.agregarCarrito("83d4ca15-0f35-48f5-b7a3-1ea210004f2e",2)
+console.log(carro)
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,5 @@
 import {renderOrder} from "./checkout/orderSummary.js";
 import {renderSummary} from "./checkout/paymentSummary.js";
-import '../data/carrito-poo.js'
+import '../data/class-carrito.js'
 renderOrder();
 renderSummary();
