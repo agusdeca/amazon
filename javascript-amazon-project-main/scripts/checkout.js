62599e0d2a2074319a855ac0412c0542db0cdc27
Agusdecaa <aguxxx10@gmail.com>
2024-08-13 00:44:50 -0300

    Se agrego archivo de practica backend

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrder} from "./checkout/orderSummary.js";
 import {renderSummary} from "./checkout/paymentSummary.js";
-import '../data/class-carrito.js'
+//import '../data/class-carrito.js'
+import '../data/practica-backend.js'
 renderOrder();
 renderSummary();
